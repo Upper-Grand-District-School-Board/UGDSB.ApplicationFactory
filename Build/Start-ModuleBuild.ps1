bfe6d2e44a3d0533b645085bdf17fd62528f26ac
Jeremy Putman <jeremy.putman@ugdsb.on.ca>
2025-08-25 14:23:43 -0400

    Update to 0.7.0

--- Build/Start-ModuleBuild.ps1
+++ Build/Start-ModuleBuild.ps1
@@ -1,5 +1,5 @@
 param(
-  [version]$global:Version = "0.6.4"
+  [version]$global:Version = "0.7.0"
 )
 #Requires -Module ModuleBuilder
 $privateFolder = Join-Path -Path $PSScriptRoot -ChildPath Source  -AdditionalChildPath "Private"
